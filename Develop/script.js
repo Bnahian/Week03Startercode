d3e06d6a026ec536236fbb124d15118a0bb77c7a
Bnahian <bnahian@gmail.com>
2022-06-23 18:09:47 -0500

    added function

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,30 @@ var generateBtn = document.querySelector("#generate");
 
 //Array to hold numbers
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
+//Array to hold uppercase letters 
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//Array to hold lowercase letters
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
-];
+//Array to hold special characters
+var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var passwordLength;
+var uppercaseCheck;
+var numberCheck;
+var specialCheck;
 
-// Created function to accept length 
+// Function created to determine the length of the password
+function determineLength() {
+    var length = prompt("Choose password length between 8 and 128 characters");
+    var askNumbers = confirm("Do you want your password to include numbers?");
+    var askLowerCase = confirm("Do you want your password to include lower case letters?");
+    var askUpperCase = confirm("Do you want your password to include upper case letters?");
+    var askSpecial = confirm("Do you want your password to include special characters?");
+
+
+
+
+}
+
 
 
 
